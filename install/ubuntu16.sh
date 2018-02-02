cfc993d2ac947ff83b8a7b67c87e1538102a38b4
Maryan Bagnyuk <marbuga@gmail.com>
2018-02-02 23:16:42 +0200

    enhance

--- install/ubuntu16.sh
+++ install/ubuntu16.sh
@@ -9,15 +9,15 @@ echo $vFileName
 echo "==== Dir name:"
 echo $vDirName
 
-echo "==== Checking vagrant dir ..."
-if [ -d /vagrant ]; then
-    echo "/vagrant dir is present. Using ..."
-    vDirName="/vagrant"
-fi
-
 echo "==== Downloads dir:"
 vDownloadsDir="$vDirName/../downloads"
 echo $vDownloadsDir
+
+echo "==== Checking vagrant dir ..."
+if [ -d /vagrant ]; then
+    echo "/vagrant dir is present. Using ..."
+    vDownloadsDir="/vagrant/downloads"
+fi
 
 echo "==== Creating downloads dir ..."
 mkdir -p $vDownloadsDir
