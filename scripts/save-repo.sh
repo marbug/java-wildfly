536f84a82a435feafc8ec4b5629843dd21ca3dcb
Maryan Bagnyuk <marbuga@gmail.com>
2018-02-02 21:48:05 +0200

    add scripts

---
+++ scripts/save-repo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "==== Checking current dir ..."
+vCurrentDir=`dirname $0`
+echo $vCurrentDir
+cd $vCurrentDir
+
+echo "==== Save to ququdev ..."
+./use-ququdev.sh
+git push
+
+echo "==== Save to bitbucket ..."
+./use-bitbucket.sh
+git push
---
+++ scripts/use-ququdev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git remote -v
+git remote set-url origin marbug@ququdev.com:/shared/git/marbug/java-windfly.git
+git remote -v
