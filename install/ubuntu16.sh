729a37b94973f0e3472e94b456b066894380d630
Maryan Bagnyuk <marbuga@gmail.com>
2018-02-04 00:36:15 +0200

    use default wildfly dir

--- install/ubuntu16.sh
+++ install/ubuntu16.sh
@@ -3,10 +3,12 @@
 vFileName="$0"
 vDirName=`dirname $vFileName`
 
-wildflyDirName="wildfly-8.2.1.Final"
+wildflyFromDir="wildfly-8.2.1.Final"
+wildflyToDir="wildfly"
 wildflyDownloadUrl="http://download.jboss.org/wildfly/8.2.1.Final/wildfly-8.2.1.Final.zip"
 jdkDirName="jdk1.8.0_161"
 programsPath="/home/ubuntu/programs"
+wildFlyToPath="/opt/$wildflyToDir"
 
 echo "==== File name:"
 echo $vFileName
@@ -39,32 +41,32 @@ echo "==== Go to downloads dir ..."
 cd $vDownloadsDir
 pwd
 
-echo "==== Checking $wildflyDirName.zip ..."
-if [ -f "./$wildflyDirName.zip" ]; then
+echo "==== Checking $wildflyFromDir.zip ..."
+if [ -f "./$wildflyFromDir.zip" ]; then
     echo "File is present"
 else
     echo "Downloading $wildflyDownloadUrl ..."
     wget -q "$wildflyDownloadUrl"
 fi
 
-echo "==== Unzipping $wildflyDirName.zip ..."
-if [ -d "$vDownloadsDir/$wildflyDirName" ]; then
-    echo "$vDownloadsDir/$wildflyDirName is present. Skipping unzip ..."
+echo "==== Unzipping $wildflyFromDir.zip ..."
+if [ -d "$vDownloadsDir/$wildflyFromDir" ]; then
+    echo "$vDownloadsDir/$wildflyFromDir is present. Skipping unzip ..."
 else
     apt-get install -y unzip
-    unzip $wildflyDirName.zip
+    unzip $wildflyFromDir.zip
 fi
 
 echo "==== Checking programs dir ..."
 echo $programsPath
 mkdir -p $programsPath
 
-echo "==== Copy $wildflyDirName to programs dir ..."
-if [ -d "$programsPath/$wildflyDirName" ]; then
-    echo "$programsPath/$wildflyDirName is present. Skipped."
+echo "==== Copy $wildflyFromDir to programs dir ..."
+if [ -d "$wildFlyToPath" ]; then
+    echo "$wildFlyToPath is present. Skipped."
 else
-    cp -R "./$wildflyDirName" "$programsPath/$wildflyDirName"
-    echo $programsPath/$wildflyDirName
+    cp -R "./$wildflyFromDir" "$wildFlyToPath"
+    echo $wildFlyToPath
 fi
 
 echo "==== Checking jdk dir ..."
@@ -81,7 +83,7 @@ echo "JAVA_HOME:" $JAVA_HOME
 echo "JAVA_HOME=\"$JAVA_HOME\"" >> /etc/environment
 
 echo "==== Setting JBOSS_HOME env variable ..."
-export JBOSS_HOME="$programsPath/$wildflyDirName"
+export JBOSS_HOME="$wildFlyToPath"
 echo "JBOSS_HOME:" $JBOSS_HOME
 echo "JBOSS_HOME=\"$JBOSS_HOME\"" >> /etc/environment
 
@@ -93,22 +95,21 @@ else
     echo $programsPath/$jdkDirName
 fi
 
-echo "==== Copy wildfly.conf to $programsPath/$wildflyDirName/bin/init.d/wildfly.conf ..."
+echo "==== Copy wildfly.conf to $wildFlyToPath/bin/init.d/wildfly.conf ..."
 echo "Old file:"
-ls -l "$programsPath/$wildflyDirName/bin/init.d/wildfly.conf"
-cp -R "$vDirName/wildfly.conf" "$programsPath/$wildflyDirName/bin/init.d/wildfly.conf"
+ls -l "$wildFlyToPath/bin/init.d/wildfly.conf"
+cp -R "$vDirName/wildfly.conf" "$wildFlyToPath/bin/init.d/wildfly.conf"
 echo "New file:"
-ls -l "$programsPath/$wildflyDirName/bin/init.d/wildfly.conf"
+ls -l "$wildFlyToPath/bin/init.d/wildfly.conf"
 
-echo "==== Copy bin/init.d/wildfly-init-debian.sh to $programsPath/$wildflyDirName/bin/init.d/wildfly.conf dir ..."
-ls -l "$programsPath/$wildflyDirName/bin/init.d/wildfly-init-debian.sh"
-cp -R "$programsPath/$wildflyDirName/bin/init.d/wildfly-init-debian.sh" "/etc/init.d/wildfly"
+echo "==== Copy bin/init.d/wildfly-init-debian.sh to $wildFlyToPath/bin/init.d/wildfly.conf dir ..."
+ls -l "$wildFlyToPath/bin/init.d/wildfly-init-debian.sh"
+cp -R "$wildFlyToPath/bin/init.d/wildfly-init-debian.sh" "/etc/init.d/wildfly"
 ls -l "/etc/init.d/wildfly"
 
 echo "==== Fixing permissions ..."
 chown -R ubuntu:ubuntu $programsPath
-# chmod -R 0777 "$programsPath/$wildflyDirName/standalone/data/content"
-chmod -R 0777 "$programsPath/$wildflyDirName"
+# chmod -R 0777 "$wildFlyToPath/standalone/data/content"
+chmod -R 0777 "$wildFlyToPath"
 
-echo "==== Reboot ..."
-reboot
+echo "==== Stop and run VM again, please !!!"
