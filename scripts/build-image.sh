d0b26efd14475d89c36c7accb352f90e1cecf7cf
Maryan Bahnyuk <marbuga@gmail.com>
2018-02-08 00:11:37 +0200

    enhance build-image.sh

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 
-docker image build -t java-wildfly .
+scriptName="$0"
+osType="$1"
 
+echo "==== Script name"
+echo $scriptName
+
+echo "==== Script directory"
+scriptDirName=`dirname $scriptName`
+echo $scriptDirName
+
+echo "==== Go to root directory"
+cd "$scriptDirName/.."
+pwd
+
+echo "==== OS type"
+echo $osType
+
+if [ "$osType" = "" ]; then
+    if [ ! -f "./Dockerfile" ]; then
+        echo "OS type is not specified"
+        echo "and Dockerfile is absent"
+        echo
+        ls -1 | grep Dockerfile
+        exit 1
+    fi
+fi
+
+docker image build -f "Dockerfile.${osType}" -t java-wildfly .
