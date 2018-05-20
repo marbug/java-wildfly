86e6b5bdaa68701ed66ae729805a3ce779dda388
Maryan Bagnyuk <marbuga@gmail.com>
2018-05-20 11:07:56 -0400

    enhance use-ququdev scripts

--- scripts/use-ququdev.bat
+++ scripts/use-ququdev.bat
@@ -1,3 +1,3 @@
 git remote -v
-git remote set-url origin marbug@ququdev.com:/shared/git/marbug/java-wildfly.git
+git remote set-url origin marbug@hk.ququdev.net:/shared/git/marbug/java-wildfly.git
 git remote -v
--- scripts/use-ququdev.sh
+++ scripts/use-ququdev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git remote -v
-git remote set-url origin marbug@ququdev.com:/shared/git/marbug/java-wildfly.git
+git remote set-url origin marbug@hk.ququdev.net:/shared/git/marbug/java-wildfly.git
 git remote -v
