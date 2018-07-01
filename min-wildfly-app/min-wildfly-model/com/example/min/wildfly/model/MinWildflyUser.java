03f7ee2a4bf749eca46c5921da2c3a44faf1311b
Maryan Bagnyuk <marbuga@gmail.com>
2018-07-01 10:15:39 -0400

    add user model

---
+++ min-wildfly-app/min-wildfly-model/com/example/min/wildfly/model/MinWildflyUser.java
@@ -0,0 +1,12 @@
+package com.example.min.wildfly.model;
+
+public class MinWildflyUser implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  
+  private Long id;
+  private String username;
+  private String password;
+  private String email;
+  
+}
